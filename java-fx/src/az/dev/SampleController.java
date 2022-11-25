ebf5b3438557d194d6ce8183de1dea7f17ab6c0f
thealkhanov <alxanli.anar@gmail.com>
2022-11-25 20:23:41 +0400

    JavaFX button və label istifadəsi

--- java-fx/src/az/dev/SampleController.java
+++ java-fx/src/az/dev/SampleController.java
@@ -1,5 +1,22 @@
 package az.dev;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class SampleController {
+	
+	@FXML
+	private TextField studentNameText;
+	
+	@FXML
+	private Label studentNameLabel;
+	
+	@FXML
+	private void saveStudent() {
+		studentNameLabel.setText("Salam, adiniz: "+studentNameText.getText());
+		
+		
+	}
 	
 }
